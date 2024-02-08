9feec229a8c365e46fe7ecb7ba615857e6274c7c
GABRIEL CANIZA <bielcaniza6@gmail.com>
2024-02-08 07:34:48 -0300

    Seller entitie class

--- src/aplication/program.java
+++ src/aplication/program.java
@@ -1,14 +1,17 @@
 package aplication;
 
+import java.util.Date;
+
 import entities.Department;
+import entities.Seller;
 
 public class program {
 
     public static void main(String[] args) {
 
         Department obj = new Department(1, "books");
-
-        System.out.println(obj);
+        Seller seller= new Seller(21, "BOB", "bob@gmail.com", new Date(), 300.00, obj);
+        System.out.println(seller);
 
     }
 
